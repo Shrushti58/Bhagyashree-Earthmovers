bba8d9810acbeb80e9c614f9b2aa26ffc2c92a67
Shrushti Patil <shrushtipatil0508@gmail.com>
2026-01-27 10:18:32 +0530

    Fix Cookie

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,16 +1,16 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (res, adminId) => {
-    const token = jwt.sign(
-        { id: adminId },
-        process.env.JWT_SECRET,
-        { expiresIn: "30m" }
-    );
+  const token = jwt.sign(
+    { id: adminId },
+    process.env.JWT_SECRET,
+    { expiresIn: "30m" }
+  );
 
-    res.cookie("token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 30 * 60 * 1000,
-    });
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 30 * 60 * 1000,
+  });
 };
